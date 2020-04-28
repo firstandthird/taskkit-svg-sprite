d502c6c5f9e8e2824bb86876e1ad6a894aa2928a
Alejandro Pérez Martín <aleperez92@gmail.com>
2020-04-28 13:42:41 +0200

    Add xmlns to generated sprite, update tests

--- index.js
+++ index.js
@@ -93,7 +93,9 @@ class SVGSpriteTask extends TaskKitTask {
           svg => svg.replace(
             '<symbol ',
             `${defs.firstChild.toString()}<symbol `
-          )
+          ).replace(
+            '<svg',
+            '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"')
         ],
       }
     };
